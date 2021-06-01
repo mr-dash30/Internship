097a4885d0661a602b33f9b3188d1b782ada1b70
Darshan Patel <65068146+mr-dash30@users.noreply.github.com>
2021-06-01 22:43:34 +0530

    Add files via upload

---
+++ day5/day5_5.py
@@ -0,0 +1,14 @@
+class Employee:
+    def info(self,name,designation):
+        print("Employee's name: ",name)
+        print("Employee's designation: ",designation)
+
+class Sal(Employee):
+    def salary(self,salary):
+        print("Salary is: ",salary)
+
+s = Sal()
+s.info("Parth","Python Developer")
+s.salary(50000)
+
+
---
+++ day5/day5_6.py
@@ -0,0 +1,18 @@
+from math import *
+
+class Cal5:
+    def _init_(self,w,l):
+        self.w = w
+        self.l = l
+
+    def calArea(self):
+         a = self.w*self.l
+
+    def display(self):
+        print("Area of Rectangle is: ",self.w*self.l)
+
+calc = Cal5(7,8)
+calc.display()
+
+
+
