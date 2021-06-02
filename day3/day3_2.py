991ceebc4a7be066c040bd8dfc5428426cbe388f
Darshan Patel <65068146+mr-dash30@users.noreply.github.com>
2021-06-02 12:55:47 +0530

    Add files via upload

---
+++ day3/day3_2.py
@@ -0,0 +1,7 @@
+x = int(input("Enter one no:"))
+
+if x%2==0:
+    print(x,"is even")
+else:
+    print(x,"is odd")
+    
---
+++ day3/day3_6.py
@@ -0,0 +1,11 @@
+ 
+num = int(input("Enter no:"))
+
+
+def fact(n):
+    ans = 1
+    for i in range(1,n+1):
+        ans = ans * i
+    return ans
+
+print(fact(num))
